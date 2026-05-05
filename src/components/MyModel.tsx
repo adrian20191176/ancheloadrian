71ceb8c0e5d30f5c9ddc2f6652f5ccc37bdf2325
Anchelo Adrian <66356323+adrian20191176@users.noreply.github.com>
2026-05-05 12:26:57 +0530

    Next JS installed

--- src/components/MyModel.tsx
+++
@@ -1,7 +0,0 @@
-import { useGLTF } from "@react-three/drei";
-
-export default function MyModel() {
-    const { scene } = useGLTF("/models/adrian.glb");
-
-    return <primitive object={scene} scale={1.5} />;
-}
